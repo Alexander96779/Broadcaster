7086db3921d7575f6838ff1ff93c926f8650a910
Alexander Prince <niyialexp@gmail.com>
2019-11-13 10:53:17 +0200

    [finishes #169709202] creating and testing add new entry endpoint

---
+++ Server/v1/controllers/entryController.js
@@ -0,0 +1,35 @@
+import entries from '../models/entries';
+import entryValidation from '../helpers/entryValidation';
+
+class entryController {
+  static createEntry(req, res) {
+    if (req.user.userType === 'user') {
+      const {
+        createdOn, title, type, location, image, video,
+      } = req.body;
+      const eId = entries.length + 1;
+      const newEntry = entryValidation.validate({
+        entryId: eId, createdOn, createdBy: req.user.id, title, type, location, status: 'draft', image, video,
+      });
+      if (!newEntry.error) {
+        entries.push(newEntry.value);
+        return res.status(201).json({
+          status: 201,
+          data: newEntry,
+          message: 'Redflag created successfully',
+        });
+      }
+      const validationError = newEntry.error.details[0].message.replace('"', ' ').replace('"', '');
+      return res.status(400).json({
+        status: 400,
+        error: validationError,
+      });
+    }
+    return res.status(403).json({
+      status: 403,
+      error: 'Only users are allowed to add entries',
+    });
+  }
+}
+
+export default entryController;
---
+++ Server/v1/models/entries.js
@@ -0,0 +1,28 @@
+const entry = [
+  {
+    entryId: 1,
+    createdOn: 12 / 12 / 2018,
+    createdBy: 2,
+    title: 'Job Corruption',
+    type: 'Redflag',
+    location: 'Muhanga',
+    status: 'Accepted',
+    // eslint-disable-next-line no-undef
+    images: [{ image: '' }, { image: '' }],
+    comment: 'This is true, it even happened where i live',
+  },
+  {
+    entryId: 2,
+    createdOn: 1 / 7 / 2019,
+    createdBy: 2,
+    title: 'Road fixing',
+    type: 'Intervention',
+    location: 'Rwamagana',
+    status: 'Under Investigation',
+    // eslint-disable-next-line no-undef
+    images: [{ image: '' }, { image: '' }],
+    comment: 'Help us please',
+  },
+];
+
+export default entry;
